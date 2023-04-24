294f6b04c87a10780f7606fb80d56a39db260fc6
Timothy Fang <timothyfang9@gmail.com>
2023-04-23 22:48:45 -0700

    added images and fixed sizing and styling

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,12 +1,13 @@
 import SplashScreen from "../components/SplashScreen.js";
 import DesignedBy from "../components/DesignedBy.js";
+import ryanPortrait from "../Photos/ryanPortrait.jpg";
 import "./LandingPage.css";
 import { useState } from "react";
 
 //ADVENTURES await the DARING soul
 const LandingPage = () => {
     const [visible, setVisible] = useState(true);
-    return(
+    return (
         <div>
             <DesignedBy />
             <div className="green-section">
@@ -14,8 +15,12 @@ const LandingPage = () => {
                     <h1 className="big-text">Adventures</h1>
                     <h1 className="medium-text">await the</h1>
                     <h1 className="big-text">Daring</h1>
-                    <h1 className="medium-text">soul</h1>
+                    <div className="image-text">
+                        <img src={ryanPortrait} alt="" className="image-box" />
+                        <h1 className="medium-text" style={{paddingLeft:"25px"}}>soul</h1>
+                    </div>
                 </div>
+
             </div>
 
             <div className="tan-section"> HELLO </div>
